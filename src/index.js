91d964af7764ec1fbcb2ab8f170daa2a813820ad
Amalie Smidth <amaliesmidth@gmail.com>
2018-11-05 16:44:23 +0000

    styling added. search bar now not case-sensitive

--- src/index.js
+++ src/index.js
@@ -11,21 +11,15 @@ const addPokemons = pokemons =>
 getPokemons()
   .then(pokemons => addPokemons(pokemons))
 
-// const searchPokemon = () => {
-//   let searchTerm = document.querySelector("#pokemon-search-input").value
-//   return  pokemonInstances.filter(pokemon => pokemon.name.includes(searchTerm))
-// }
-
 const searchPokemon = () => {
-  let searchTerm = document.querySelector("#pokemon-search-input").value
-  let matches = pokemonInstances.filter(pokemon => pokemon.name.includes(searchTerm))
-  //delete non-matches from page
+  let searchTerm = document.querySelector("#pokemon-search-input").value.toLowerCase()
+  let matches = pokemonInstances.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm))
+  //clear non-matches from page
   document.querySelector("#pokemon-container").innerHTML = ""
-  //clear search form
-  // document.querySelector("#pokemon-search-input").value = ""
   //add matches to page
   return matches.forEach(pokemon => pokemon.addPokemon())
 }
+
 
 pokemonSubmitForm.addEventListener("keyup", event => {
   event.preventDefault()
